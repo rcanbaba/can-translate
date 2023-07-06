f81d95d22a5d6199680956417ddc91b69e0687c6
rcanbaba <rcanbabaoglu@gmail.com>
2023-07-06 22:50:07 +0300

    first view created.

--- can-translate/can-translate/MainViewController.swift
+++ can-translate/can-translate/MainViewController.swift
@@ -11,10 +11,27 @@ import SnapKit
 
 class MainViewController: NSViewController {
     
+    private lazy var selectedView: NSView = {
+        let view = NSView()
+        view.wantsLayer = true
+        view.layer?.backgroundColor = NSColor.red.cgColor
+        view.layer?.borderColor = NSColor.yellow.cgColor
+        view.layer?.borderWidth = 1.0
+        view.layer?.cornerRadius = 25.0
+        return view
+    }()
+    
     override func loadView() {
         view = NSView(frame: NSMakeRect(0.0, 0.0, 800.0, 600.0))
         let label = NSTextField(labelWithString: "Another Controller")
         view.addSubview(label)
+        
+        view.addSubview(selectedView)
+        selectedView.snp.makeConstraints { make in
+            make.width.equalTo(100)
+            make.height.equalToSuperview()
+            make.center.equalToSuperview()
+        }
     }
     
     override func viewDidLoad() {
